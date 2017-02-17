173cb15e5ef99ea13527039730dd53557061afdd
Roman Tronenko <roman@italki.com>
2017-02-18 00:19:31 +0800

    default params and stuff; still requires ton of refactoring, we know... soon there'll be time for it

--- gogo-upload.go
+++ gogo-upload.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	defaultInputDir      = "."
+	defaultOutputDir     = defaultInputDir
 	defaultAuthor        = "gogo"
 	defaultPlace         = "chushangfeng"
 	defaultWatermarkPath = "watermarks/gogo-watermark.png"
@@ -20,14 +21,14 @@ var inputDir string
 
 func init() {
 	flag.Header("General")
-	flag.Add("i", "inputDir", "directory with newly made tattoo photos", ".")
-	flag.Add("o", "outputDir", "directory with newly made tattoo photos", "~/output")
+	flag.Add("i", "inputDir", "directory with newly made tattoo photos", defaultInputDir)
+	flag.Add("o", "outputDir", "directory with newly made tattoo photos", "[same with input]")
 	flag.Header("Watermark")
-	flag.Add("w", "watermark", "watermark image file", "watermarks/gogo.png")
+	flag.Add("w", "watermark", "watermark image file", defaultWatermarkPath)
 	flag.Add("l", "needLabels", "set true if you want labels added on watermark", false)
-	flag.Add("d", "labelDate", "date on the label, default is file's update date", "")
-	flag.Add("a", "labelMadeAt", "the name of the place it was made at", "chushangfeng")
-	flag.Add("b", "labelMadeBy", "the name of the artist", "gogo")
+	flag.Add("d", "labelDate", "date on the label, default is file's update date", "[file date]")
+	flag.Add("a", "labelMadeAt", "the name of the place it was made at", defaultPlace)
+	flag.Add("b", "labelMadeBy", "the name of the artist", defaultAuthor)
 
 	// Default values
 	watermark.WatermarkPath = defaultWatermarkPath
@@ -52,6 +53,10 @@ func init() {
 		case "outputDir":
 			watermark.OutputDir = opt.String()
 		}
+	}
+
+	if len(watermark.OutputDir) == 0 {
+		watermark.OutputDir = inputDir
 	}
 
 	//dirPath = flag.Remainder
