4b4dbb7964ef5809246433a68efc88f267e4bfe1
Roman Tronenko <roman@italki.com>
2017-03-26 19:40:09 +0800

    attempting to restart ipfsdaemon if it's down

--- cli/cli.go
+++ cli/cli.go
@@ -3,8 +3,10 @@ package cli
 import (
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gogotattoo/gogo-upload/watermark"
 	gia "github.com/ipfs/go-ipfs-api"
@@ -23,7 +25,28 @@ func addWatermarkAndUpload(path string, fi os.FileInfo, err error) error {
 		} else {
 			outputPath = watermark.AddWatermark(path, watermark.MakeWatermark(file, path))
 		}
-		hash, _ := sh.AddDir(outputPath)
+		hash, er := sh.AddDir(outputPath)
+		if er != nil {
+			log.Println("Error", er)
+		}
+		if len(hash) == 0 {
+			log.Println("ipfs api is not running... attempting to launch it")
+			daemonCmd := exec.Command("ipfs", "daemon")
+			daemonCmd.Stdout = os.Stdout
+			daemonCmd.Stderr = os.Stderr
+			go daemonCmd.Start()
+			for len(hash) == 0 {
+				time.Sleep(time.Second * 20)
+				log.Print("Trying after 20 seconds... ")
+				sh = gia.NewShell("localhost:5001")
+				hash, er = sh.AddDir(outputPath)
+				if er != nil {
+					log.Println("Error", er)
+				}
+				log.Println("Hash: ", hash)
+			}
+			log.Println("Ok")
+		}
 		hashes = append(hashes, hash)
 		os.Rename(outputPath, outputPath[:len(outputPath)-4]+"_"+hash+".JPG")
 		log.Println("Hash: ", hash)
